89ae29cae0227e1d322f360d96dc3f956949772a
Piotr Nowiński <pitred007@gmail.com>
2019-05-19 22:58:51 +0200

    Update Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -3,37 +3,13 @@ var Contact = React.createClass({
         item: React.PropTypes.object.isRequired,
     },
     render: function () {
-        return ( <
-            div className = {
-                'contactItem'
-            } >
-            <
-            img className = {
-                'contactImage'
-            }
-            src = {
-                'https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/120px-User_icon_2.svg.png'
-            }
-            /> <
-            p className = {
-                'contactLabel'
-            } > Name: {
-                this.props.item.firstName
-            } < /p> <
-            p className = {
-                'contactLabel'
-            } > Last Name: {
-                this.props.item.lastName
-            } < /p> <
-            a className = {
-                'contactEmail'
-            }
-            href = {
-                'mailto: ' + this.props.item.email
-            } > {
-                this.props.item.email
-            } < /a> <
-            /div>
+        return ( 
+            <div className = {'contactItem'}>
+                <img className = {'contactImage'} src = {'https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/120px-User_icon_2.svg.png'}/>
+                <p className = {'contactLabel'}> Name: {this.props.item.firstName}</p>
+                <p className = {'contactLabel'}> Last Name: {this.props.item.lastName}</p>
+                <a className = {'contactEmail'} href = {'mailto: ' + this.props.item.email}> {this.props.item.email}</a>
+            </div>
         )
     },
 });
