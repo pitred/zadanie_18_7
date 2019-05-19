7bf2b1509c1fdf33fa97f73f021fba4168f26e23
Piotr Nowiński <pitred007@gmail.com>
2019-05-19 23:20:48 +0200

    Update Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -3,7 +3,7 @@ var Contact = React.createClass({
         item: React.PropTypes.object.isRequired,
     },
     render: function () {
-        return ( 
+        return (
             <div className = {'contactItem'}>
                 <img className = {'contactImage'} src = {'https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/120px-User_icon_2.svg.png'}/>
                 <p className = {'contactLabel'}> Name: {this.props.item.firstName}</p>
