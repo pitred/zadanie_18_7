89019d5d90cdae7fd73510a6b8d6a53e2a7c2da6
Piotr Nowiński <pitred007@gmail.com>
2019-05-19 23:00:39 +0200

    Update ContactForm.js

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -3,31 +3,13 @@ var ContactForm = React.createClass({
         contact: React.PropTypes.object.isRequired
     },
     render: function () {
-        return ( <
-            form className = {
-                'contactForm'
-            } >
-            <
-            input type = 'text'
-            placeholder = 'Name'
-            value = {
-                this.props.contact.firstName
-            }
-            /> <
-            input type = 'text'
-            placeholder = 'Last Name'
-            value = {
-                this.props.contact.lastName
-            }
-            /> <
-            input type = 'email'
-            placeholder = 'Email'
-            value = {
-                this.props.contact.email
-            }
-            /> <
-            button type = 'submit' > Add contact < /button> <
-            /form>
+        return ( 
+            <form className = {'contactForm'}>
+                <input type = 'text' placeholder = 'Name' value = {this.props.contact.firstName}/>
+                <input type = 'text' placeholder = 'Last Name' value = {this.props.contact.lastName}/>
+                <input type = 'email' placeholder = 'Email' value = {this.props.contact.email}/>
+                <button type = 'submit' > Add contact < /button>
+            </form>
         )
     },
 })
